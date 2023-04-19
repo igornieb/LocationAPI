14faa1b5d4bc8dff8ba31243162fa07f641a7263
igornieb <igornieb@gmail.com>
2023-04-19 12:18:27 +0200

    added location column

--- models/Place.py
+++ models/Place.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Boolean, String, Integer, UUID
+from sqlalchemy import Column, Boolean, String, Integer, UUID, Float
+from geoalchemy2 import Geography, WKTElement
 import uuid
 from database import Base
 from sqlalchemy.orm import relationship
@@ -11,4 +12,15 @@ class Place(Base):
     name = Column(String, nullable=False)
     description = Column(String, nullable=False)
     published = Column(Boolean, server_default='FALSE', nullable=False, default=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    location = Column(Geography('POINT', srid=4326, spatial_index=True), nullable=False)
     # created_by = relationship(User)
+
+    def __init__(self, name, description, published, latitude, longitude):
+        self.name = name
+        self.description = description
+        self.published = published
+        self.latitude = latitude
+        self.longitude = longitude
+        self.location = WKTElement(f'POINT({longitude} {latitude})', srid=4326)
