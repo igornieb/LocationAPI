69c20e2ab3595089973e4144ad4590860200d246
igornieb <igornieb@gmail.com>
2023-04-19 16:30:09 +0200

    added pagination

--- routers/category.py
+++ routers/category.py
@@ -9,11 +9,14 @@ router = APIRouter(
     tags=['Categories']
 )
 
+paginate_by = 100
+
 #TODO protect routes
 
 @router.get("/")
-def get_all(db: Session = Depends(get_db)):
-    return db.query(Category).all()
+def get_all(db: Session = Depends(get_db), page: int = 1):
+    offset = (page - 1) * paginate_by
+    return db.query(Category).offset(offset).paginate_by(paginate_by) .all()
 
 @router.post("/", response_model=schemas.Category)
 def get_all(category:schemas.CategoryCreate, db: Session = Depends(get_db)):
