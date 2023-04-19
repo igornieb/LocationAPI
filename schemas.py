fae4a3129a87ee81054b849d5c6c581774872d9f
igornieb <igornieb@gmail.com>
2023-04-19 17:55:32 +0200

    added avg score for Place, Category and Opinion schemas

--- schemas.py
+++ schemas.py
@@ -1,10 +1,17 @@
+import uuid
+from typing import Optional, List
 from pydantic import BaseModel
+
+from models.Place import Place
 
 
 class PlaceBase(BaseModel):
     name: str
     description: str
+    category: str
     published: bool = False
+    latitude: float
+    longitude: float
 
 
 class PlaceCreate(PlaceBase):
@@ -16,7 +23,39 @@ class PlaceEdit(PlaceBase):
 
 
 class Place(PlaceBase):
-    id: int
+    id: uuid.UUID
+    avg_score: float
+
+    class Config:
+        orm_mode = True
+
+
+class CategoryBase(BaseModel):
+    category: str
+
+
+class CategoryCreate(CategoryBase):
+    pass
+
+
+class Category(CategoryBase):
+    id: uuid.UUID
+
+    class Config:
+        orm_mode = True
+
+
+class OpinionBase(BaseModel):
+    stars: int
+    opinion: str
+
+
+class OpinionCreate(OpinionBase):
+    place: uuid.UUID
+
+
+class Opinion(OpinionCreate):
+    id: uuid.UUID
 
     class Config:
         orm_mode = True
