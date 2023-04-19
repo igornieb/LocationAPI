76955bc86291eafcf5d16bd3ffd5f359209a3d53
igornieb <igornieb@gmail.com>
2023-04-19 17:54:55 +0200

    made avg_score hybrid_property

--- models/Place.py
+++ models/Place.py
@@ -1,9 +1,15 @@
-from sqlalchemy import Column, Boolean, String, Integer, UUID, Float, ForeignKey
+from sqlalchemy import Column, Boolean, String, Integer, UUID, Float, ForeignKey, func
 from geoalchemy2 import Geography, WKTElement
 import uuid
 from database import Base
-from models.Category import Category
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Session
+from database import engine
+from models.Opinion import Opinion
+from sqlalchemy.ext.hybrid import hybrid_method, hybrid_property
+
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker(bind=engine)
 
 
 class Place(Base):
@@ -16,7 +22,6 @@ class Place(Base):
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
     location = Column(Geography('POINT', srid=4326, spatial_index=True), nullable=False)
-    # created_by = relationship(User)
     category = Column(String, ForeignKey('category.category', ondelete="CASCADE"))
 
     def __init__(self, name, description, published, latitude, longitude, category):
@@ -27,3 +32,14 @@ class Place(Base):
         self.longitude = longitude
         self.category = category
         self.location = WKTElement(f'POINT({longitude} {latitude})', srid=4326)
+
+    @hybrid_property
+    def avg_score(self):
+        session = Session()
+        avg = session.query(func.avg(Opinion.stars)).filter(Opinion.place == self.id).one()
+        session.close()
+        if str(avg[0])=='None':
+            return float(0.0)
+        else:
+            return float(avg[0])
+
