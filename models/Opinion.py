47652480cf3df69e72c832c3dccd490ba63b96fd
igornieb <igornieb@gmail.com>
2023-04-19 16:29:37 +0200

    created opinion model

---
+++ models/Opinion.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String, Integer, UUID, CheckConstraint, ForeignKey
+from geoalchemy2 import Geography, WKTElement
+import uuid
+from database import Base
+
+
+class Opinion(Base):
+    __tablename__ = "opinion"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    stars = Column(Integer, CheckConstraint('stars>0 and stars<=5'), nullable=False)
+    opinion = Column(String, nullable=False)
+    place = Column(UUID(as_uuid=True), ForeignKey('place.id', ondelete="CASCADE"))
