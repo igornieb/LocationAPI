24572326ceb44e2148d17cb7307de38128c77d31
igornieb <igornieb@gmail.com>
2023-04-20 19:49:09 +0200

    removed todos

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
-from routers import place
-from models import Place
-from database import engine
+from fastapi import FastAPI, Depends
+from routers import place_router, category_router, opinion_router, user_router
+from models import Place, Category, Opinion, User
+from database import engine, get_db
 
-#TODO migrations, models, coordinates fields, opinions
+Category.Base.metadata.create_all(bind=engine)
 Place.Base.metadata.create_all(bind=engine)
-#
+Opinion.Base.metadata.create_all(bind=engine)
+User.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(place.router)
+app.include_router(place_router.router)
+app.include_router(category_router.router)
+app.include_router(opinion_router.router)
+app.include_router(user_router.router)
+
